4b2daedf55c4baf5614da1c76b87520e2ccfd9a4
BobDuncan7 <bob.experience@outlook.com>
2020-01-06 08:30:15 +0100

    add src

---
+++ src/defaultConfig.mjs
@@ -0,0 +1,10 @@
+export const config = {
+  semi: false,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+}
