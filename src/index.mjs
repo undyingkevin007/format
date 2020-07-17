f28793a9bff1cd3c1928fe33b3577de531cb5ad7
BobDuncan7 <bob.experience@outlook.com>
2020-07-17 20:39:35 +0200

    do not error if .gitignore does not exist

--- src/index.mjs
+++ src/index.mjs
@@ -17,16 +17,23 @@ export const format = async args => {
     include = [cwd]
   }
 
-  const gitignore = await fs.readFile(path.join(cwd, '.gitignore'), 'utf8')
-  const gitignoreArray = gitignore.split('\n').filter(a => a)
+  let exclude
 
-  let exclude = gitignoreArray
-  if (args.exclude) {
-    if (is.array(args.exclude)) {
-      exclude = [...exclude, ...args.exclude]
-    } else {
-      exclude = [...exclude, args.exclude]
+  const gitIgnorePath = path.join(cwd, '.gitignore')
+
+  if (await fs.exists(gitIgnorePath)) {
+    const gitignore = await fs.readFile(gitIgnorePath, 'utf8')
+    exclude = gitignore.split('\n').filter(a => a)
+
+    if (args.exclude) {
+      if (is.array(args.exclude)) {
+        exclude = [...exclude, ...args.exclude]
+      } else {
+        exclude = [...exclude, args.exclude]
+      }
     }
+  } else {
+    exclude = ['node_modules', 'coverage']
   }
 
   exclude = Array.from(new Set(exclude))
