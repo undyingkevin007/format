96d2d47a04ed7db2b4ba42de22413e4a98e38160
BobDuncan7 <bob.experience@outlook.com>
2022-01-24 16:23:05 +0100

    test/defaultConfig: use strings instead of functions

--- test/defaultConfig.mjs
+++ test/defaultConfig.mjs
@@ -2,14 +2,14 @@ import { is, version } from '@magic/test'
 import { config } from '../src/defaultConfig.mjs'
 
 export const spec = {
-  semi: is.bool,
-  printWidth: is.int,
-  tabWidth: is.int,
-  useTabs: is.bool,
-  singleQuote: is.bool,
-  trailingComma: is.string,
-  bracketSpacing: is.bool,
-  arrowParens: is.string,
+  semi: 'bool',
+  printWidth: 'int',
+  tabWidth: 'int',
+  useTabs: 'bool',
+  singleQuote: 'bool',
+  trailingComma: 'string',
+  bracketSpacing: 'bool',
+  arrowParens: 'string',
 }
 
 export default version(config, spec)
